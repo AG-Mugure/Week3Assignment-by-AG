5280e283f8d264c7a9e6783721fe306afb5556c4
AG-Mugure <151904224+AG-Mugure@users.noreply.github.com>
2025-08-27 15:16:41 +0300

    Add files via upload

---
+++ Week3Assignment.py
@@ -0,0 +1,17 @@
+#Create a function to calculate discount
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+        discount_amount = price * (discount_percent / 100)
+        return price - discount_amount
+    else:
+        return price
+
+# Prompt the user for input
+original_price = float(input("Enter the original price of the item: "))
+discount = float(input("Enter the discount percentage: "))
+
+# Calculate the final price
+final_price = calculate_discount(original_price, discount)
+
+# Print the final price
+print(f"The final price is: {final_price}")
